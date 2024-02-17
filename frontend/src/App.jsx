48b3953f07de6ab88cb2046df35a8aea905303b4
CommitTODev <abhishekshivale45@gmail.com>
2024-02-17 08:09:04 +0530

    404 page added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Page404 from './pages/404'
+function App() {
+  return (
+    <div className='text-3xl'>
+      <Page404 />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Page404Svg from '../../public/Page404Svg.svg'
+function Page404() {
+  return (
+    <div>
+    <div className="min-h-screen flex items-center justify-center w-10/12 m-auto">
+        <div className="hidden md:flex md:w-1/2 p-8">
+           <img src={Page404Svg} alt="" />
+        </div>
+
+        <div className="w-full md:w-1/2 p-4 md:p-8">
+            <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">404 Not Found</h1>
+            <p className="text-lg md:text-xl text-gray-600 mb-8">Oops! The page you are looking for could not be found.</p>
+            <a href="/" className="px-6 py-3 bg-blue-500 text-white rounded-lg inline-block hover:bg-blue-600 transition duration-300 ease-in-out">Go to Home</a>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Page404
