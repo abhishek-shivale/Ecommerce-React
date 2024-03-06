8c36afe3122e58f0d2d11e1a8a3b6911f12f4b67
CommitTODev <abhishekshivale45@gmail.com>
2024-03-06 19:58:24 +0530

    url changed done

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-react-dta.pages.dev",
     credentials: true,
   })
 );
@@ -19,3 +19,5 @@ app.use("/api/v1", UserRouter);
 app.use("/api/v1/product", productRouter);
 
 export default app;
+// http://localhost:5173
+//origin: "https://ecommerce-react-dta.pages.dev",
